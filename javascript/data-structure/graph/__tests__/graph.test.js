4128347a8eb4e51d0157e1299e15096f73247186
afnandamra <damra.afnan@gmail.com>
2021-05-23 13:36:36 +0300

    CC36: breadth first graph

--- javascript/data-structure/graph/__tests__/graph.test.js
+++ javascript/data-structure/graph/__tests__/graph.test.js
@@ -1,6 +1,7 @@
 'use strict';
 
-let { Graph, Vertex } = require('../graph');
+const Graph = require('../graph');
+const Vertex = require('../vertex');
 
 describe('Graph Test', () => {
   const graph = new Graph();
@@ -49,81 +50,5 @@ describe('Graph Test', () => {
     for (const [vertex] of nodes) {
       expect(vertex).toHaveProperty('value');
     }
-  });
-});
-
-describe('Graph Breadth Test', () => {
-  it('should return all nodes breadth first properly', () => {
-    const graph = new Graph();
-
-    const ten = new Vertex(10);
-    const two = new Vertex(2);
-    const six = new Vertex(6);
-    const three = new Vertex(3);
-    const seven = new Vertex(7);
-    const eight = new Vertex(8);
-
-    const expectedSet = new Set();
-    expectedSet.add(ten);
-    expectedSet.add(two);
-    expectedSet.add(six);
-    expectedSet.add(three);
-    expectedSet.add(seven);
-    expectedSet.add(eight);
-
-    graph.addVertex(ten);
-    graph.addVertex(two);
-    graph.addVertex(six);
-    graph.addVertex(three);
-    graph.addVertex(seven);
-    graph.addVertex(eight);
-
-    graph.addEdge(ten, two);
-    graph.addEdge(ten, six);
-    graph.addEdge(ten, three);
-    graph.addEdge(ten, seven);
-    graph.addEdge(two, seven);
-    graph.addEdge(six, seven);
-    graph.addEdge(six, eight);
-    graph.addEdge(three, eight);
-    graph.addEdge(eight, seven);
-    // expect(graph.breadth(ten)).toEqual([10, 2, 6, 3, 7, 8]);
-    expect(graph.breadth(ten)).toEqual(expectedSet);
-  });
-  it('should return all nodes breadth first properly', () => {
-    const graph = new Graph();
-
-    const Pandora = new Vertex('Pandora');
-    const Arendelle = new Vertex('Arendelle');
-    const Metroville = new Vertex('Metroville');
-    const Monstroplolis = new Vertex('Monstroplolis');
-    const Narnia = new Vertex('Narnia');
-    const Naboo = new Vertex('Naboo');
-
-    const expectedSet = new Set();
-    expectedSet.add(Pandora);
-    expectedSet.add(Arendelle);
-    expectedSet.add(Metroville);
-    expectedSet.add(Monstroplolis);
-    expectedSet.add(Narnia);
-    expectedSet.add(Naboo);
-
-    graph.addVertex(Pandora);
-    graph.addVertex(Arendelle);
-    graph.addVertex(Metroville);
-    graph.addVertex(Monstroplolis);
-    graph.addVertex(Narnia);
-    graph.addVertex(Naboo);
-
-    graph.addEdge(Pandora, Arendelle);
-    graph.addEdge(Arendelle, Metroville);
-    graph.addEdge(Arendelle, Monstroplolis);
-    graph.addEdge(Metroville, Monstroplolis);
-    graph.addEdge(Metroville, Narnia);
-    graph.addEdge(Metroville, Naboo);
-    graph.addEdge(Monstroplolis, Naboo);
-    graph.addEdge(Narnia, Naboo);
-
-    expect(graph.breadth(Pandora)).toEqual(expectedSet);
   });
 });
--- javascript/data-structure/graph/graph.js
+++ javascript/data-structure/graph/graph.js
@@ -1,19 +1,6 @@
 'use strict';
 
-const { Queue } = require('../stacksAndQueues/stacks-and-queues');
-
-class Vertex {
-  constructor(value) {
-    this.value = value;
-  }
-}
-
-class Edge {
-  constructor(vertex, weight = 0) {
-    this.vertex = vertex;
-    this.weight = weight;
-  }
-}
+const Edge = require('./edge');
 
 class Graph {
   constructor() {
@@ -26,7 +13,10 @@ class Graph {
   }
 
   addEdge(startVertex, endVertex, weight) {
-    if (!this._adjacencyList.has(startVertex) || !this._adjacencyList.has(endVertex)) {
+    if (
+      !this._adjacencyList.has(startVertex) ||
+      !this._adjacencyList.has(endVertex)
+    ) {
       return 'INVALID NODE';
       // throw new Error('INVALID NODE');
     } else {
@@ -59,49 +49,6 @@ class Graph {
       console.log([vertex, edge]);
     }
   }
-
-  // Breadth-first: uses a queue to track where you've been.
-  breadth(startingNode) {
-    if (!startingNode) return 'INVALID NODE';
-    // Our queue class
-    const queue = new Queue();
-    // A set is an object that will only store unique keys.
-    const visitedNodes = new Set();
-
-    // let result = [];
-
-    queue.enqueue(startingNode);
-    visitedNodes.add(startingNode);
-    // result.push(startingNode.value);
-
-    while (!queue.isEmpty()) {
-      // Remove the first item from the queue.
-      const currentNode = queue.dequeue();
-      // Get the neighbors of the current node.
-      const neighbors = this.getNeighbors(currentNode);
-      // Loop over all the neighbors.
-      for (let neighbor of neighbors) {
-        const neighborNode = neighbor.vertex;
-        if (visitedNodes.has(neighborNode)) {
-          // If the Set has the node that I'm looking for, continue on to the next node.
-          continue;
-        } else {
-          // Otherwise, I haven't been there and I need to add it to the Set.
-          visitedNodes.add(neighborNode);
-          // Put it into the queue
-          queue.enqueue(neighborNode);
-          // result.push(neighborNode.value);
-        }
-      }
-    }
-    // console.log(result);
-    // Return the Set, so that there is a list of all visited nodes.
-    return visitedNodes;
-  }
 }
 
-module.exports = {
-  Vertex,
-  Edge,
-  Graph,
-};
+module.exports = Graph;
